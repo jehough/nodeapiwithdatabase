9eb61d21536f188c4e05a8a19503fb6967e0a793
jehough <teachlife@outlook.com>
2020-04-25 08:13:35 -0400

    adds get rout to characters

--- models/character.js
+++ models/character.js
@@ -2,7 +2,7 @@ mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const characterSchema = new Schema({
-    name: "String",
+    name: {type: "String", unique: "true"},
     movie: "String"
 })
 
--- routes/characters.js
+++ routes/characters.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Character = require('../models/character')
 
 
 router.get('/', function(req, res, next){
-    res.json({"Find the Character": "Yoda"})
+    Character.find()
+        .then(char => {
+            res.send(char)
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        })
 })
 
 module.exports = router;
