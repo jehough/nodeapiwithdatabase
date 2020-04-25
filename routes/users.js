9305e29ecd83352acdf679816a834c262a20c287
jehough <teachlife@outlook.com>
2020-04-25 14:26:59 -0400

    adds routes to user

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a number');
 });
 
+router.post('/', function(req, res, next){
+  
+})
+
 module.exports = router;
---
+++ secrets.js
@@ -0,0 +1,1 @@
+const accessTokenSecret = 'SCrncilapekcDOiva;lkdnp'
